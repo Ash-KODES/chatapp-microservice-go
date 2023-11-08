82aefa5b86e30a762b9690b85ea1aba7fc2be44f
kamtendra <skamtendra@gmail.com>
2023-11-08 16:35:21 +0530

    messege to socket

--- message-service/handler/message-handler.go
+++ message-service/handler/message-handler.go
@@ -3,60 +3,76 @@
 package handler
 
 import (
-    "encoding/json"
-    "net/http"
-    "database/sql"
-    "mychatapp/models"
-    "mychatapp/repository"
+	"database/sql"
+	"encoding/json"
+	"mychatapp/models"
+	"mychatapp/repository"
+	"net/http"
 )
 
 type MessageHandler struct {
-    repo *repository.MessageRepository
+	repo *repository.MessageRepository
 }
 
 func NewMessageHandler(db *sql.DB) *MessageHandler {
-    return &MessageHandler{repository.NewMessageRepository(db)}
+	return &MessageHandler{repository.NewMessageRepository(db)}
 }
 
 // message_http_handler.go
 
 func (h *MessageHandler) CreateMessageHandler(w http.ResponseWriter, r *http.Request) {
-    var message models.Message
-    decoder := json.NewDecoder(r.Body)
-    if err := decoder.Decode(&message); err != nil {
-        http.Error(w, "Invalid request payload", http.StatusBadRequest)
-        return
-    }
-    defer r.Body.Close()
+	var message models.Message
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&message); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
 
-    // You should validate and authenticate the user, obtain the channel ID, and perform necessary checks.
+	// You should validate and authenticate the user, obtain the channel ID, and perform necessary checks.
 
-    // Send the message to the WebSocket channel for real-time delivery
-    // For example, if you have a WebSocket connection for the channel, you can send the message to that connection.
+	// Send the message to the WebSocket channel for real-time delivery
+	// For example, if you have a WebSocket connection for the channel, you can send the message to that connection.
+	h.sendToWebSocketChannel(message)
 
-    // Insert the message into the database with the associated channel ID
-    // You should extract the channel ID from the message or the user's session.
+	// Insert the message into the database with the associated channel ID
+	// You should extract the channel ID from the message or the user's session.
 
-    messageID, err := h.repo.CreateMessage(message)
-    if err != nil {
-        http.Error(w, "Error creating message", http.StatusInternalServerError)
-        return
-    }
+	messageID, err := h.repo.CreateMessage(message)
+	if err != nil {
+		http.Error(w, "Error creating message", http.StatusInternalServerError)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(struct {
-        MessageID int `json:"message_id"`
-    }{MessageID: messageID})
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(struct {
+		MessageID int `json:"message_id"`
+	}{MessageID: messageID})
+}
+
+// sendToWebSocketChannel sends a message to the WebSocket channel
+func (h *MessageHandler) sendToWebSocketChannel(message models.Message) {
+	// Assuming you have a WebSocket connection associated with the channel
+	// You may need to implement the logic for sending the message to the WebSocket connection
+	// Here, we use the WebSocket handler to send the message to the WebSocket connection
+	if h.ws != nil {
+		messagePayload, err := json.Marshal(message)
+		if err != nil {
+			// Handle JSON encoding error
+			return
+		}
+		h.ws.SendToChatPartner(websocket.TextMessage, messagePayload)
+	}
 }
 
 // message_http_handler.go
 
 func (h *MessageHandler) GetMessagesHandler(w http.ResponseWriter, r *http.Request) {
-    // Extract the channel ID from the request, validate user access, and obtain messages for that channel from the database.
+	// Extract the channel ID from the request, validate user access, and obtain messages for that channel from the database.
 
-    // Retrieve messages for the specified channel
-    // You should adjust the logic to match your data schema and security requirements.
+	// Retrieve messages for the specified channel
+	// You should adjust the logic to match your data schema and security requirements.
 
-    // Return the messages as a JSON response
+	// Return the messages as a JSON response
 }
